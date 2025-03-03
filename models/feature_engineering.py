4e1d15e1608f14d634a4c3ea0eb7588ecde00f46
Arx <maralroj@bancolombia.com.co>
2025-03-02 21:51:46 -0500

    Update depl

--- models/feature_engineering.py
+++ models/feature_engineering.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.functions import col
 
+
 spark = SparkSession.builder.appName('FeatureEngineering').getOrCreate()
 df = spark.read.csv('dbfs:/mnt/datasets/dataset.csv', header=True, inferSchema=True)
 assembler = VectorAssembler(inputCols=['edad', 'ingreso'], outputCol='features')
